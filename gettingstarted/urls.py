d49c5d4b0a9428ab7b63542bfda16d0e608d7e26
dwanderson <anderson.dan.w@gmail.com>
2016-07-23 14:26:43 -0400

    Adding initial formulation for an About Me page, to make this feel like a real website

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^aboutme/', hello.views.aboutme, name='aboutme'),
     url(r'^blogs/$', hello.views.blogs, name='blogs'),
     ## eg: /blogs/0
     url(r'^blogs/(?P<blogIndex>[0-9]+)/', hello.views.nblog, name='some_blog'),
